e10c86ffc2159e7bcb27253e00638d4822574a93
jessicafarias <jessica.farias.rosado@gmail.com>
2020-10-13 12:47:09 -0500

    Adding empty models
    Adding User model
    Adding Opinion model
    Adding Following model

---
+++ db/migrate/20201013174226_create_opinions.rb
@@ -0,0 +1,10 @@
+class CreateOpinions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :opinions do |t|
+      t.text :body
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
